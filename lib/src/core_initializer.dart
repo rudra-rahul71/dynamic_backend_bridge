83c55a503b1e7089afcb1fe56b15506ac80ef3e2
Rahul Rudra <rahul@appreeze.com>
2026-07-04 13:47:52 +0530

    chore: bump version to 0.0.2 and fix unused import

--- lib/src/core_initializer.dart
+++ lib/src/core_initializer.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
